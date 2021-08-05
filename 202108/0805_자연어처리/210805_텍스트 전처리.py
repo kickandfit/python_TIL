99411e8bd63e0b97722214aeefc72950d05d129b
HO <moonwh112@naver.com>
2021-08-05 17:48:30 +0900

    210805|one-hot encoding by keras

--- 202108/0805_자연어처리/210805_텍스트 전처리.py
+++ 202108/0805_자연어처리/210805_텍스트 전처리.py
@@ -417,19 +417,87 @@ for s in sentences:
 print(encoded)
 # -
 
+# ## 원-핫 인코딩(one-hot encoding)
+#
+# - 매우 중요
+# - 단어 집합의 크기를 벡터의 차원으로 하고, 표현하고 싶은 단어의 인덱스에 1의 값을 부여하고, 다른 인덱스에 0을 부여하는 단어의 벡터 표현 방식
+#     1. 각 단어에 고유한 인덱스를 부여합니다(cf. 정수 인코딩)
+#     2. 표현하고 싶은 단어의 인덱스의 위치에 1을 부여하고, 다른 단어의 인덱스에 0 을 부여한다
+
+# 'barber' 'secret' 'hug' 'kept' 'person'
+#    1        0       0     0       1  ----> 첫번째 문장
+#    1        0       1     0       1  ----> 두번째 문장
+
+# +
+from konlpy.tag import Okt  
+okt=Okt()  
+token=okt.morphs("나는 자연어 처리를 배운다")  
+print(token)
+
+word2index={}
+for voca in token:
+     if voca not in word2index.keys():
+            word2index[voca]=len(word2index)
+print(word2index)
+
+def one_hot_encoding(word, word2index):
+    one_hot_vector = [0]*(len(word2index))
+    index=word2index[word]
+    one_hot_vector[index]=1
+    return one_hot_vector
+
+
+# -
+
+from konlpy.tag import Okt  
+okt=Okt()  
+token=okt.morphs("나는 자연어 처리를 배운다")  
+print(token)
+
+
+word2index={}
+for voca in token:
+     if voca not in word2index.keys():
+            word2index[voca]=len(word2index)
+print(word2index)
+
+
+def one_hot_encoding(word, word2index):
+    one_hot_vector = [0]*(len(word2index))
+    index=word2index[word]
+    one_hot_vector[index]=1
+    return one_hot_vector
 
 
 
+one_hot_encoding("자연어",word2index)
 
+# ####  케라스(Keras)를 이용한 원-핫 인코딩(One-Hot Encoding)
+#
+# - to_categorical()을 지원
+# - 정수 인코딩과 원-핫 인코딩을 순차적으로 진행
 
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
 
+# +
+text="나랑 점심 먹으러 갈래 점심 메뉴는 햄버거 갈래 갈래 햄버거 최고야"
 
+t = Tokenizer()
+t.fit_on_texts([text])
+print(t.word_index) # 각 단어에 대한 인코딩 결과 출력
+# 갈래가 3개니까 맨앞 점심 2 햄버거 2 그러나 순서가 점심이 빠르니 2번
+# 이런식으로 다해줌
+# -
 
+sub_text="점심 먹으러 갈래 메뉴는 햄버거 최고야"
+encoded = t.texts_to_sequences([sub_text])[0]
+print(encoded)
 
-
-
-
-
-
+one_hot = to_categorical(encoded)
+print(one_hot)
+# 인덱스는 0부터 붙음
+# 따라서 처음 : 점심 인덱스 2번 벡터 1 나머지 0
+# 다음 : 먹으러 인덱스 5번 벡터 1 나머지 0
 
 

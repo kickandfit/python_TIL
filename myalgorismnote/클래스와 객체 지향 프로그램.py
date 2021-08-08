7e7cf3dc5dfb083da16e496e495b879f53dcd685
HO <moonwh112@naver.com>
2021-08-08 22:00:44 +0900

    210808| 클래스 이해하는 중

--- myalgorismnote/클래스와 객체 지향 프로그램.py
+++ myalgorismnote/클래스와 객체 지향 프로그램.py
@@ -341,7 +341,389 @@ import copy
 xs = [[1,2,3],[4,5,6],[7,8,9]]
 zs = copy.deepcopy(xs)
 
+
 # #### 의미
 # - 내장 컬렉션의 경우 간단히 리스트, 딕셔너리, 세트의 팩터리 함수를 이용하여 얕은 복사본을 사용하는 것이 더 파이썬다운것
+
+# #### 임의의 객체 복사하기
+#
+#
+# - copy.copy() : 얕은 복사
+# - copy.deepcopy() : 깊은 복사
+#
+
+# # !r은 repr에 적용할때 사용함
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __repr__ (self):
+        return f'Point({self.x!r}, {self.y!r})'
+
+
+
+# +
+import copy
+
+a = Point(23, 42)
+b = copy.copy(a)
+
+b
+# -
+
+a is b
+
+
+# #### 이 부분에서의 핵심
+# - 점(point) 객체는 좌표에 원시 타입(정수형)을 사용하기 때문에 이 경우 얕은 복사와 깊은 복사에는 차이가 없음
+
+# +
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __repr__ (self):
+        return f'Point({self.x!r}, {self.y!r})'
+    
+class Rectangle:
+    def __init__(self, topleft, bottomright):
+        self.topleft = topleft
+        self.bottomright = bottomright
+        
+    def __repr__(self):
+        return (f'Rectangle({self.topleft!r},' f'{self.bottomright!r})')
+    
+rect = Rectangle(Point(0,1), Point(5,6))
+srect = copy.copy(rect)
+
+rect
+# -
+
+srect
+
+rect is srect
+
+rect.topleft.x = 999
+srect
+
+drect = copy.deepcopy(rect)
+drect.topleft.x =222
+drect
+
+srect
+
+rect
+
+
+# #### keypoint ( 객체 복사하기 )
+#
+# - 객체의 얕은 복사본을 만들면 자식 객체가 복제되지 않는다. 따라서 사본은 원본과 완전히 독립적이지 않다
+# - 깊은 복사는 객체의 자식 객체를 재귀적으로 복사한다. 복사본과 원본은 완전히 독립적이지만 시간이 더 걸린다
+# - copy 모듈을 사용하여 임의의 객체를 복사할 수 있다
+
+# # 추상화 클래스는 상속을 확인한다
+#
+# - 추상화 클래스는 파생 클래스가 기반 클래스의 특정 메서드를 구현함을 보장함
+# - 추상화 클래스의 이점과 파이썬에 내장된 abc 모듈로 추상화 클래스를 정의하는 방법
+#
+
+# #### 유지 보수하기 좋도록 확인 사항
+# - 기본 클래스를 인스턴스화하는 것은 불가능
+# - 서브클래스에서 인터페이스 메서드를 구현하는 것을 잊어버리면 가능한 빨리 에러를 발생시킴
+
+# +
+class Base:
+    def foo(self):
+        raise NotImplementedError()
+    def bar(self):
+        raise NotImplementedError()
+
+class Concrete(Base):
+    def foo(self):
+        return 'foo() called'
+    
+    # 이런, bar()를 재정의하는 걸 잊어버렸네
+    # def bar(self):
+    #     return "bar() called"
+
+
+# -
+
+# #### 방금 코드가 던져주는 의미
+#
+# - Base의 인스턴스에서 메서드를 호출하면 NotImplementedError 가 올바르게 발생함
+
+b = Base()
+b.foo()
+
+c = Concrete()
+c.foo()
+
+c.bar()
+
+# #### 첫 번째 구현은 좋지만 완벽하지 않음
+#
+# - Base를 인스턴스화해도 에러가 나지 않는다.
+# - 불완전한 서브클래스를 제공한다. Concrete를 인스턴스화해도 누락된 bar() 메서드를 호출하기 전에는 에러를 발생시키지 않음
+
+# +
+from abc import ABCMeta, abstractmethod
+
+class Base(metaclass = ABCMeta):
+    @abstractmethod
+    def foo(self):
+        pass
+    
+    @abstractmethod
+    def bar(self):
+        pass
+    
+class Concrete(Base):
+    def foo(self):
+        pass
+    
+    # bar()를 선언하는 걸 또 잊어버렸다.
+    
+assert issubclass(Concrete, Base)
+
+c = Concrete()
+# -
+
+# ####  이렇게 작성하면 유용한 점 ( 위의 코드 )
+# - Base의 서브클래스에서 추상 메서드 구현을 깜박했다면
+# - '인스턴스화할 때' TypeError를 발생시김
+# - 또한 발생한 예외는 누락된 메서드가 무엇인지 알려줌
+# - abc 없이는 누락된 메서드가 실제로 호출된 경우에만 NotImplementedError를 발생
+
+# #### keypoint( 추상화 클래스 ABC ( Abstract Base Class))
+#
+# - 추상화 클래스는 파생 클래스가 인스턴스화될 때 기반 클래스의 추상 메서드를 모두 구현하였는지 확인한다.
+# - 추상화 클래스를 사용하면 버그를 방지하고 클래스 계층을 쉽게 유지 관리 할 수 있다.
+
+# # 네임드튜플은 어디에 적합한가
+# - 네임드튜플 컨테이너 타입이 있음
+# - 네임드튜플은 클래스를 수동으로 정의하는 데 대한 훌륭한 대안
+# - 기본 tuple 데이터 타입의 확장
+#     - tuple 은 임의의 객체를 그룹화하기 위한 간단한 데이터 구조
+#     - 불변이기에 생성되면 수정할 수 없음
+#     - 튜플에 저장된 개별 속성에는 이름을 지정할 수 없음
+#     - 튜플에는 같은 수의 필드와 같은 속성이 담겨 있다고 보장하지 못함
+#     - 필드 순서를 혼동해서 '실수로 인한 버그'가 자주 발생
+
+tup = ('hello', object(), 42)
+tup
+
+tup[2] = 23
+
+# #### 해결사 네임트 튜플
+# - 변경 불가능한 컨테이너
+# - 최상위 속성에 데이터를 저장한 후 속성을 업데이트하여 데이터를 수정할 수 없음
+# - 한 번만 쓰고 여러번 읽음의 원칙을 따름
+# - 말 그대로 이름 지어진 튜플
+
+# - 첫 번째 인자 매개 변수는 타입명이라고 하며 namedtuple 함수를 호출하여 생성되는 새 클래스의 이름
+# - namedtuple은 우리가 결과 클래스를 할당할 변수명을 알 수 없기 때문에 사용할 클래스 이름을 명시적으로 알려 줘야함
+# - 이 클래스명은 namedtuple이 개발자를 위해 자동으로 생성하는 독스트링과 __ repr __ 구현에 사용됨
+#
+
+# +
+from collections import namedtuple
+
+Car1 = namedtuple('Car', 'color mileage')
+
+# -
+
+'color mileage'.split()
+Car2 = namedtuple('Car1323', ['color', 'mileage'])
+my_car = Car2('red', 3812.4)
+my_car.mileage
+
+my_car[0]
+
+tuple(my_car)
+
+# +
+color, mileage = my_car
+print( color, mileage)
+
+print(*my_car)
+print(str(my_car))
+# -
+
+my_car
+
+my_car.color = 'blue'
+
+# #### 네임드튜플 정리
+# - 내부적으로 일반 파이썬 클래스로 구현
+# - 메모리 사용량도 일반 클래스보다 '좋음'
+# - "네임드튜플은 파이썬에서 불변 클래스를 수동으로 정의할 때 사용할 수 있는 메모리 활용의 효율적인 지름길"
+
+# ### 네임드튜플 상속하기
+#
+# - 네임드튜플은 일반 파이썬 클래스 위에서 만들어지기 때문에 네임드튜플 객체에 메서드 추가 가능
+
+# +
+from collections import namedtuple
+
+Car = namedtuple('Car', 'color mileage')
+
+class MyCarWithMethods(Car):
+    def hexcolor(self):
+        if self.color =='red':
+            return '#ff0000'
+        else:
+            return '#000000'
+        
+c = MyCarWithMethods('red', 1234)
+c.hexcolor()
+# -
+
+# #### 위 코드의 문제점
+# - 다소 거추장 스러움
+# - 불변 속성들로 구성된 클래스를 원한다면 할 만한 가치가 있을지도 모름
+# - 새로운 '불변' 필드를 추가하기가 까다로움
+# - 네임드튜플 계층을 만드는 가장 쉬운 방법은 기본 튜플의_fields 속성을 사용하는 것
+
+Car = namedtuple('Car', 'color mileage')
+ElectricCar = namedtuple('ElectricCar', Car._fields + ('charge', ))
+
+ElectricCar('red', 1234, 45.0)
+
+# #### 내장 도우미 메서드
+#
+# - _ fields 속성 외에도 각 네임드튜플 인스턴스는 유용한 몇 가지 도우미 메서드를 제공함
+# - _ 로 시작함
+# - 이 문자는 보통 해당 메서드나 속성이 클래스 또는 모듈의 안정된 공개 인터페이스의 일부가 아니라 '프라이빗'임을 뜻함
+# - 네임드튜플의 경우 밑줄 명명 규칙은 다른 의미를 가짐
+# - 네임드튜플에서는 밑줄로 시작하는 도우미 메서드 및 속성은 네임드튜플의 공개 인터페이스에 포함됨
+# - 사용자 정의 튜플 필드와의 충돌을 피하기 위해 밑줄을 사용했을 뿐
+#
+
+# ####  네임드 튜플 딕셔너리로 변경하기
+
+my_car._asdict()
+
+import json
+json.dumps(my_car._asdict())
+
+# #### 네임드 튜플 (얕은) 복사본을 생성하고 필드의 일부 선택적 대체
+
+my_car._replace(color='blue')
+
+# #### 네임드 튜플 객체 시퀀스나 반복 가능 객체로부터 새로운 네임드튜플 인스턴스 생성
+
+Car._make(['red',999])
+
+
+# #### 네임드튜플을 사용하면 좋은 경우
+#
+# - 데이터를 잘 구조화하여 코드를 정돈하고 가독성이 더 좋게함
+# - 딕셔너리처럼 형식이 고정된 임시 데이터 타입에서 네임드튜프로 바꾸면 개발자의 의도를 좀더 명확하게 표현 가능
+# - 리팩터링을 시도하다 보면 자신이 직면한 문제에 대해 고민해볼 수있음
+# - 구조화되지 않은 튜플과 딕셔너리 대신 네임드튜플을 사용하면 동료가 작업을 더 쉽게 수행할 수 있게 함
+# - '더 깔끔하고' 유지 보수가 쉬운 코드를 작성하는 데 도움이 되지 않는 경우에는 네임드튜플 사용을 자제
+
+# #### keypoint( 네임드튜플 )
+#
+# - collections.namedtuple은 불변 클래스를 수동으로 정의하는 메모리 효율적 지름길
+# - 네임드 튜플은 데이터를 이해하기 쉬운 구조로 만들어 코드를 정리하는 데 도움을 줌
+# - 네임트튜플은 유용한 도우미 메서드를 제공
+#     - _ make, _ replace, _ asdict()
+
+# # 클래스 변수 대 인스턴스 변수의 함정
+#
+# - 클래스 메서드와 인스턴스 메서드를 구별하는 것 외에도 파이썬의 객체 모델은 클래스아 인스턴스의 '변수'를 구별함
+# - 파이썬 객체, '클래스 변수'와 '인스턴스 변수'라는 두가지 데이터 속성이 존재
+# - 클래스 변수
+#     - 클래스 정의 안에(그러나 인스턴스 메스드 밖에) 선언
+#     - 특정 클래스 인스턴스에 묶여 있지 않음
+#     - 클래스 자체에 내용을 저장, 같은 클래스에서 생성된 모든 객체는 동일한 클래스 변수 집합을 공유함
+#     - 클래스변수를 수정하면 동시에 모든 객체 인스턴스에 영향을 미침
+#
+# - 인스턴스 변수
+#     - 항상 특정 객체 인스턴스에 묶여 있음
+#     - 그 내용은 클래스에 저장되지 않음 클래스에서 생성된 개별 객체에 저장됨
+#     - 인스턴스 변수의 내용은 객체 인스턴스마다 완전히 독립적
+#     - 해당 객체에만 영향을 미침
+
+class Dog:
+    num_legs = 4 # <-클래스 변수
+    
+    def __init__(self, name):
+        self.name = name # <- 인스턴스 변수
+        
+
+
+# #### 새 Dog 인스턴스를 만들면 각 인스턴스는 name이라는 인스턴스 변수를 얻음
+
+jack = Dog('jack')
+jill = Dog('jill')
+print( jack.name, jill.name)
+
+print(jack.num_legs, jill.num_legs)
+
+# #### 각 Dog 인스턴스 또는 '클래스 자체에서' num_legs에 접근 가능
+
+jack.num_legs, jill.num_legs
+
+Dog.num_legs
+
+# #### 클래스르 통해 인스턴스 변수에 접근하려고 하면 에러 (Attribute Error)
+#
+# - 인스턴스 변수는 각 객체에 특정되고 __ init __ 생성자가 실행될 때 마다 생성
+# - 클래스 자체에는 존재하지 않음
+#
+#     - 이것이 클래스 변수와 인스턴스 변수의 주요 차이점
+
+Dog.name
+
+# #### 클래스 변수를 바꾸면 모든 인스터스에 영향을 미침
+
+# +
+# 클래스 변수 바꾸기
+
+Dog.num_legs = 6
+# -
+
+jack.num_legs, jill.num_legs
+
+# +
+# 인스턴스에서 클래스 변수에 해당하는 값 바꿔보기
+Dog.num_legs=4
+jack.num_legs= 6
+
+jack.num_legs, jill.num_legs, Dog.num_legs
+# -
+
+# #### 위의 코드
+#
+# - 원하는 결과를 얻은 것 같이 보임
+# - 문제는 잭 인스턴스에 num_legs 인스턴스 변수를 도입했다는 점
+# - 새로운 num_legs 인스턴스 변수는 동일한 이름의 클래스 변수를 '가려' 객체 인스턴스 범위에 접글할 때 이를 무시하고 숨김
+
+# 마지막 말 설명 예제 코드
+jack.num_legs, jack.__class__.num_legs
+
+
+# - 즉 이 클래스 변수는 '동기화 되지 않는' 것처럼 보임
+# - 그 이유는 jack.num_legs에 값을 써서 클래스 변수와 똑같은 이름의 인스턴스 변수가 만들어졌기 때문
+# - 나쁜 것은 아니지만 이 뒤에서 무슨 일이 일어나는 것인지는 인지해야함
+#     - 사실은, 객체 인스턴스를 통해 클래스 변수를 수정하려고 시도하는 것, 즉 실수로 이름이 같은 인스턴스 변수를 만들어 원래 클래스 변수를 가리는 것은 객체 지향프로그래밍의 함정
+
+# #### 개가 없는 예제
+#
+
+# - CountedObject는 공유 카운터 역할을 하는 num_instances 를 클래스 변수를 갖고, 클래스가 선언되면 카운터가 0으로 초기화된 채로 있음
+
+class CountedObject:
+    num_instances = 0
+    
+    def __init__(self):
+        self.__class__.num_instances += 1
+
+
 
 

baf1c6c38b1680fc3388916ea1181c9594117432
HO <moonwh112@naver.com>
2021-09-13 16:51:40 +0900

    210913 bfs 관련 문제 풀이 추가

--- practice/복슬할 때 사용하는 공간.py
+++ practice/복슬할 때 사용하는 공간.py
@@ -1878,6 +1878,66 @@ participant = ['ㅁ', 'ㄴ', 'ㅇ','ㄹ','ㄹ']
 completion =  ['ㅁ', 'ㄴ', 'ㅇ','ㄹ']
 answer = collections.Counter(participant) - collections.Counter(completion)
 answer
-# -
+# +
+# 프로그래머스 네트워크
+# 이건 안돼네 왜 안되는 지 생각 좀 해보자
+
+computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+parent = [0]*(len(computers)+1)
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a> b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+for i in range(1, len(computers)+1):
+    parent[i] = i
+    
+for i in range(len(computers)):
+    for j in range(len(computers)):
+        if computers[i][j] == 1:
+#             print( i, ' ',j)
+            union_parent(parent, i+1 , j+1)
+result = 0
+
+for i in range(1, len(parent)):
+    if i == 1:
+        result += 1
+        continue
+    if parent[i] == parent[i-1]:
+        result += 1
+print(result)
 
 
+# +
+#이건 bfs인데 되네
+# 그렇지 이건 dfs나 bfs 둘다 상관없지
+# 그 부분만 다 탐색해서 하나 올려주는 것과 같은 이치니까
+
+def solution(n, computers):
+    answer = 0
+    bfs = []
+    visited = [0]*n
+
+    while 0 in visited:
+        x = visited.index(0)
+        bfs.append(x)
+        visited[x] = 1
+        
+        while bfs:
+            node = bfs.pop(0)
+            visited[node] = 1
+            for i in range(n):
+                if visited[i] == 0 and computers[node][i] == 1:
+                    bfs.append(i)
+                    visited[i] = 1
+        answer += 1
+    return answer
